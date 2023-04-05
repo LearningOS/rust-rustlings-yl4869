1b9be954e8ae6596bca7e287d99b6c157a13ca9c
yunlanglang <yunlanglang@outlook.com>
2023-04-05 20:16:38 +0800

    update

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -5,11 +5,8 @@
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
+    if let Some(x) = option {
         res += x;
     }
-    // while let Some(x) = option {
-    //     res += x;
-    // }
     println!("{}", res);
 }
